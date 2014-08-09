7ac04c78db1167c4b33ce1970f7fd8145b55fc7c
christophebriguet <christophe.briguet@swissonline.ch>
2014-08-09 12:20:35 +0000

    Finish layout and routes

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, :type => :controller do
+describe StaticPagesController, :type => :controller do
 
   describe "GET home" do
     it "returns http success" do
@@ -12,6 +12,20 @@ RSpec.describe StaticPagesController, :type => :controller do
   describe "GET help" do
     it "returns http success" do
       get :help
+      expect(response).to be_success
+    end
+  end
+
+    describe "GET about" do
+    it "returns http success" do
+      get :about
+      expect(response).to be_success
+    end
+  end
+  
+    describe "GET contact" do
+    it "returns http success" do
+      get :contact
       expect(response).to be_success
     end
   end
