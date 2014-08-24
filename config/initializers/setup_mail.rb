6d48d66b9f964bc8bcf87545b12b0c0f4e468ee7
christophebriguet <christophe.briguet@swissonline.ch>
2014-08-24 15:09:52 +0000

    Development Mail Interceptor

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,5 @@
+require 'development_mail_interceptor'
+
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.hispeed.ch",
   :port                 => 25,
@@ -8,6 +10,6 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+#ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 
-#ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
